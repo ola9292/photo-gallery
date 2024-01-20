940c66c901c8fcb06beb9d4aadb7244999c6f19b
Ben Wain <ben@vektor.co.uk>
2024-01-20 19:06:39 +0000

    changes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\PhotosController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/albums');
 });
 
 Route::get('/albums', [AlbumsController::class, 'index'])->name('albums.index');
